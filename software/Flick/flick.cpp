9c6d4db792ab1e0004b86febcf8cfc7a294f0d44
Boyd Timothy <btimothy@gmail.com>
2025-06-22 21:43:17 -0600

    Update copyright year for Flick

--- software/Flick/flick.cpp
+++ software/Flick/flick.cpp
@@ -1,5 +1,5 @@
 // Flick for Funbox DIY DSP Platform
-// Copyright (C) 2024 Boyd Timothy <btimothy@gmail.com>
+// Copyright (C) 2025 Boyd Timothy <btimothy@gmail.com>
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
