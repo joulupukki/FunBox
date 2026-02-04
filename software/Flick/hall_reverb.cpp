160a5c9ce18e407b33be79ee7bda7ce81055d0b5
Boyd Timothy <btimothy@gmail.com>
2026-02-03 17:15:43 -0700

    Update file headers

--- software/Flick/hall_reverb.cpp
+++ software/Flick/hall_reverb.cpp
@@ -1,5 +1,20 @@
-// Hall Reverb implementation
-// Copyright (C) 2025 Boyd Timothy
+// Flick for Funbox DIY DSP Platform
+// Copyright (C) 2026 Boyd Timothy <btimothy@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #include "hall_reverb.h"
 
--- software/Flick/spring_reverb.h
+++ software/Flick/spring_reverb.h
@@ -1,7 +1,23 @@
-// Spring Reverb implementation for FunBox
+// Flick for Funbox DIY DSP Platform
+// Copyright (C) 2026 Boyd Timothy <btimothy@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 // Digital waveguide model of spring reverb
-// Emulates Fender Deluxe Reverb sound
-// Copyright (C) 2025 Boyd Timothy
+// Goal: Emulate 1960'ss Fender Deluxe Reverb
 
 #ifndef SPRING_REVERB_HPP
 #define SPRING_REVERB_HPP
